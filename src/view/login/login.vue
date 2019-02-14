57ea844e5d2f7290f8a208f8794b0e564d1abf61
DZF <dengzhifeng_63@163.com>
2019-02-14 13:20:57 +0800

    登录权限判断

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -18,69 +18,72 @@
 </template>
 
 <script>
-    import LoginForm from '_c/login-form'
-    import config from '@/libs/api.request'
-    import {mapActions} from 'vuex'
+import LoginForm from '_c/login-form'
+import config from '@/libs/api.request'
+import { mapActions } from 'vuex'
 
-    export default {
-        components: {
-            LoginForm
-        },
-        data() {
-            return {
-                loading: false
-            }
-        },
-        methods: {
-          goRegister () {
-            let URL = config.baseUrl.split('api')[0]
-            window.open(URL + 'register');
-          },
-            ...mapActions([
-                'handleLogin',
-                'getRoutersConfig'
-            ]),
-            handleSubmit({userName, password}) {
-                const _this = this;
-                _this.loading = true;
-                _this.handleLogin({userName, password}).then(res => {
-                  console.log(res)
-                    if (res.code === 1) {
-                        setTimeout(() => {
-                            _this.loading = false;
-                            _this.$Message.error(res.data.message || '系统错误');
-                        }, 1000);
-                    } else if (res.code === 2) {
-                      setTimeout(() => {
-                        _this.loading = false;
-                        _this.$Message.error(res.data.message || '你尚未注册');
-                      }, 1000);
-                    } else {
-                        _this.$Message.loading({
-                            content: '登录系统中...',
-                            duration: 1.5,
-                            onClose: function() {
-                                _this.loading = false;
-                                Promise.all([_this.getRoutersConfig()]).then(res => {
-                                    // 注册新路由配置
-                                    _this.$router.addRoutes(res[0])
-                                    _this.$router.push({
-                                        name: 'home'
-                                    })
-                                });
-                            },
-                        });
-                    }
-                }).catch(err => {
-                    setTimeout(() => {
-                        console.log('err', err)
-                        _this.loading = false;
-                         _this.$Message.error(err.response.data.message || '系统错误');
-                    }, 1000);
-                })
-            }
-        }
+export default {
+  components: {
+    LoginForm
+  },
+  data () {
+    return {
+      loading: false
     }
+  },
+  methods: {
+    goRegister () {
+      let URL = config.baseUrl.split('api')[0]
+      window.open(URL + 'register')
+    },
+    ...mapActions([
+      'handleLogin',
+      'getRoutersConfig'
+    ]),
+    handleSubmit ({ userName, password }) {
+      const _this = this
+      _this.loading = true
+      _this.handleLogin({ userName, password }).then(res => {
+        console.log(res)
+        if (res.code === 1) {
+          setTimeout(() => {
+            _this.loading = false
+            _this.$Message.error(res.data.message || '系统错误')
+          }, 1000)
+        } else if (res.code === 2) {
+          setTimeout(() => {
+            _this.loading = false
+            _this.$Message.error(res.data.message || '你尚未注册')
+          }, 1000)
+        } else {
+          if (res.data.is_admin !== 1) {
+            return _this.$Message.error('抱歉！你没有权限访问后台系统')
+          }
+          _this.$Message.loading({
+            content: '登录系统中...',
+            duration: 1.5,
+            onClose: function () {
+              _this.loading = false
+              Promise.all([_this.getRoutersConfig()]).then(res => {
+                // 注册新路由配置
+                _this.$router.addRoutes(res[0])
+                _this.$router.push({
+                  name: 'home'
+                })
+              })
+            }
+          })
+        }
+      }).catch(err => {
+        setTimeout(() => {
+          console.log('err', err)
+          _this.loading = false
+          _this.$Message.error(err.response.data.message || '系统错误')
+        }, 1000)
+      })
+    }
+  }
+}
 </script>
 
 <style>
