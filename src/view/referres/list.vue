e7f8670256e595c8e1f75beea0bf9c2227fd5394
DZF <dengzhifeng_63@163.com>
2019-05-14 10:52:51 +0800

    测试活动分页

--- src/view/referres/list.vue
+++ src/view/referres/list.vue
@@ -22,19 +22,17 @@
     </Card>
     <Tabs @on-click="getTab" style="margin-top: 12px;">
       <TabPane label="推荐人列表"  name="score">
-        <Col span="24">
           <Input
             v-model="searchKeyword"
             @on-enter="handleSearch"
             placeholder="关键字搜索..."
-            style="width: 200px; margin-bottom: 22px;"/>
+            style="width: 160px; margin-bottom: 22px;"/>
           <span @click="handleSearch" >
                     <Button type="primary" icon="ios-search" style="margin-left: 12px; margin-bottom: 22px;">搜索</Button>
                 </span>
           <Table :loading="loading" :columns="orgColumns" :data="information" style="width: 100%;" border></Table>
           <Page :total="orgTotal" @on-change="handlePage" :page-size="15"
                 style="float:right;margin-top:5px;margin-bottom:30px;"></Page>
-        </Col>
       </TabPane>
     </Tabs>
     <!--<Modal-->
