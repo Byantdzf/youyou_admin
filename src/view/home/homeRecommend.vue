4e47490afddfe6d214055c6a57e319c0837977b1
web260320689@163.com <web260320689@163.com>
2019-06-04 10:07:37 +0800

    测试

--- src/view/home/homeRecommend.vue
+++ src/view/home/homeRecommend.vue
@@ -1,274 +1,329 @@
-
 <template>
-  <div class="hello">
-    <!--<Menu mode="horizontal" theme="light" active-name="1">-->
-    <!--<MenuItem name="1">-->
-    <!--<Icon type="ios-settings" />-->
-    <!--首页配置-->
-    <!--</MenuItem>-->
-    <!--</Menu>-->
-    <Card style="margin-top: 18px;">
-      <p slot="title" style="color: #ff6c4c;">管理员操作</p>
-      <div style="display: inline-block;margin-left: 22px;width: 100%;">
-        <Col span="9">
-          <Card>
-            <!--<dropdown :dropData="redMun" v-on:getGropData="getGropData"  title="请选择用户"  style="display: inline-block;margin-bottom: 12px;"></dropdown>-->
-            <Select v-model="client_id" style="width: 300px;" filterable @on-query-change="getGropData">
-              <Option v-for="item in redMun" :value="item.id" :key="item.id">{{ item.name }}</Option>
-            </Select>
-            <!--<Input placeholder="请搜索用户" style="width: 300px;" />-->
-            <Button type="success" style="display: inline-block; margin-left: 12px;" @click="allocation">增加征婚推荐</Button>
-            <uploadImage v-on:uploadPictures="uploadPictures" style="margin-top: 12px;"></uploadImage>
-          </Card>
-          <div>
-                        <span v-for="item,index in information" :key="index"
-                              style="padding: 22px;border-bottom: 2px solid #f6f6f6;display: inline-block;">
-                            <img :src="item.photo" alt="" width="100" style="box-shadow: 0px 0px 12px #d3d3d3;">
-                            <span style="font-size: 16px;font-weight: bold;margin-left: 22px;">用户名：{{item.name}}</span>
-                            <Button type="error" style="margin-left: 22px" @click="deleteUser(item.id)">删除</Button>
-                        </span>
-          </div>
-        </Col>
-        <Col span="12" offset="1">
-          <Card>
-            <p slot="title" style="color: #3bc84d;">首页通知</p>
-            <Row>
-              <Col span="24" v-for="item,index in messageList" :key="index"
-                   style="margin-top: 12px;padding-bottom: 12px;border-bottom: 2px solid #f0f0f0;">
-                <Col span="11">
-                  <div  @click="getIndex(index)">
-                    <informPic v-on:uploadPictures="uploadInformPic" :pic="item.pic"></informPic>
-                  </div>
-                  <Input v-model="item.title" type="textarea" placeholder="Enter message..."/>
-                </Col>
-                <Col span="12" offset="1">
-                  <Input v-model="item.path" placeholder="跳转路径（path）" style="width: 300px"/>
-                  <Select v-model="item.type" style="width: 300px;margin-top: 8px;">
-                    <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
-                  </Select>
-                  <DatePicker type="daterange" v-model="item.date" placement="bottom-end" placeholder="通知显示时间" style="width: 220px;margin-top: 8px
-;margin-right: 12px"></DatePicker>
-                  <Button type="error" @click="removeMessage(index)">删除</Button>
-                </Col>
-              </Col>
-            </Row>
-            <div style="text-align: center;margin-top: 22px;">
-              <Button style="margin: auto;float: left" type="dashed" @click="addMessage">添加+</Button>
-              <Button type="success" size="large" icon="ios-checkmark" @click="saveMessage">保存</Button>
-            </div>
-          </Card>
-        </Col>
-      </div>
+  <div v-model="activeTab">
+    <Card>
+      <Tabs @on-click="getTab">
+        <TabPane label="未处理" name="0">
+          <Input
+            v-model="searchKeyword"
+            @on-enter="handleSearch"
+            placeholder="关键字搜索..."
+            style="width: 200px; margin-bottom: 22px;"/>
+          <span @click="handleSearch">
+                    <Button type="primary" icon="ios-search" style=" margin-bottom: 22px;margin-left: 12px;">搜索</Button>
+                </span>
+          <Table :loading="loading" :columns="orgColumns" :data="information" style="width: 100%;" border></Table>
+          <Page :total="orgTotal" @on-change="handlePage" :page-size="15"
+                style="float:right;margin-top:20px;margin-bottom:20px;"></Page>
+        </TabPane>
+        <TabPane label="已处理" name="1">
+          <Input
+            v-model="searchKeyword"
+            @on-enter="handleSearch"
+            placeholder="关键字搜索..."
+            style="width: 200px; margin-bottom: 22px;"/>
+          <span @click="handleSearch">
+                    <Button type="primary" icon="ios-search" style=" margin-bottom: 22px;margin-left: 12px;">搜索</Button>
+                </span>
+          <Table :loading="loading" :columns="orgColumns" :data="information" style="width: 100%;" border></Table>
+          <Page :total="orgTotal" @on-change="handlePage" :page-size="15"
+                style="float:right;margin-top:20px;margin-bottom:20px;"></Page>
+        </TabPane>
+      </Tabs>
     </Card>
+    <Modal
+      v-model="modal"
+      title="温馨提示"
+      :ok-text="text"
+      @on-ok="cancel"
+    >
+      <p style="font-size: 14px;font-weight: bold;margin-bottom: 4px">拒绝理由：</p>
+      <i-input style="width: 340px" type="textarea" :rows="3" placeholder="请输入..." v-model="content"></i-input>
+    </Modal>
+    <Modal
+      v-model="modal2"
+      title="温馨提示"
+      no-cancel>
+      <div style="font-size: 16px">
+
+      </div>
+    </Modal>
   </div>
 </template>
 
 <script>
-  import uAxios from '../../api'
-  import uploadImage from '../components/uploadImage'
-  import dropdown from '../components/dropdown'
+import uAxios from '../../api/index'
+import config from '../../api/config'
 
-  export default {
-    name: 'Org',
-    components: {
-      dropdown: dropdown,
-      uploadImage: uploadImage,
-      informPic: uploadImage
-    },
-    data () {
-      return {
-        articlesId: '',
-        switch1: false,
-        redMun: [],
-        disabled: false,
-        user_is_admin: 0,
-        information: [],
-        loading: false,
-        VIPinformation: [],
-        poster: '',
-        client_id: 0,
-        searchKeyword: '',
-        photo: '',
-        activeTab: 'orgInfo',
-        informIndex: 0,
-        character: {},
-        uploaddata: [],
-        cityList: [
-          {
-            value: 'OF',
-            label: '公众号'
+export default {
+  search: '',
+  name: 'complain',
+  data() {
+    return {
+      activeTab: '0',
+      currentPage: 1,
+      searchKeyword: '',
+      orgTotal: 0,
+      modal2: false,
+      type: '',
+      id: '',
+      modal1: false,
+      text: '确认',
+      content: '',
+      orgColumns: [
+        {
+          title: 'ID',
+          align: 'center',
+          width: 100,
+          key: 'recommend_id'
+        },
+        {
+          title: '用户ID',
+          align: 'center',
+          width: 120,
+          key: 'user_id'
+        },
+        {
+          title: '名称',
+          key: 'user_name',
+          align: 'center',
+        },
+        {
+          title: '头像',
+          key: 'photo',
+          render: (h, params) => {
+            return h('img', {
+              attrs: {
+                src: params.row.photo
+              },
+              style: {
+                width: '42px',
+                height: '42px',
+                marginTop: '6px',
+              },
+              on: {
+                click: () => {
+                  let argu = {id: params.row.user_id}
+                  this.$router.push({
+                    name: 'user_detail',
+                    params: argu
+                  })
+                }
+              }
+            })
           },
-          {
-            value: 'MP',
-            label: '小程序'
-          }
-        ],
-        messageList: [] // 通知数量
-      }
-    },
-    methods: {
-      uploadPictures (image) {
-        this.photo = image // 轮播
-      },
-      uploadInformPic (image) {
-        console.log(this.informIndex)
-        this.messageList[this.informIndex].pic = image // 轮播
-      },
-      getIndex (index) {
-        this.informIndex = index
-      },
-      getGropData (value) {
-        console.log(this.searchKeyword)
-        let self = this
-        self.loading = true
-        uAxios.get(`admin/users?type=single&keyword=${value}`)
-          .then(res => {
-            let result = res.data.data.data
-            this.redMun = result.map((item) => {
-              return {
-                name: item.name,
-                id: item.id
-              }
-            })
-            console.log(this.redMun)
-          })
-      },
-      addMessage () {
-        // 增加message数据
-        this.messageList.push({
-          title: '',
-          date: [],
-          start_time: '',
-          end_time: '',
-          path: '',
-          type: '',
-          pic: ''
-        })
-      },
-      removeMessage (index) {
-        // 删除message数据
-        this.$Notice.warning({
-          title: '温馨提示',
-          desc: '记得保存之后才会生效哦！'
-        })
-        this.messageList.splice(index, 1)
-      },
-      returnDate (date) {
-        // 格式化时间 return
-        const d = new Date(date)
-        var resDate = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds()
-        return resDate.split(' ')[0]
-      },
-      saveMessage () {
-        console.log(this.messageList)
-        for (let item of this.messageList) {
-          item.start_time = this.returnDate(item.date[0])
-          item.end_time = this.returnDate(item.date[1])
-          for (let key in item) {
-            if (item[key].toString().replace(/(^\s*)|(\s*$)/g, '') == '') {
-              return this.$Message.error('请填写完整信息后再保存！')
-            }
-          }
-        }
-        let data = {
-          announcement: this.messageList
-        }
-        uAxios.post(`admin/announcements`, data).then((response) => {
-          if (response.data.code === 0) {
-            this.$Message.info('设置成功')
-          } else {
-            this.$Modal.error({
-              content: response.data.message
-            })
-          }
-        })
-      },
-      gotoUrl (name, name_id, id) {
-        let argu = {}
-        argu[name_id] = id
-        this.$router.push({
-          name: name,
-          params: argu
-        })
-      },
-      // delete用户
-      deleteUser (id) {
-        let test_value = confirm('你确定要继续吗？'),
-          self = this
-        if (test_value) {
-          uAxios.delete(`admin/home/recommends/${id}`).then((response) => {
-            if (response.data.code === 0) {
-              this.$Message.info('删除成功')
-              this.getlist(this.currentPage)
+          align: 'center'
+        },
+        {
+          title: '手机号',
+          align: 'center',
+          key: 'mobile'
+        },
+        {
+          title: '性别',
+          key: 'sex',
+          align: 'center',
+        },
+        {
+          title: '单身/介绍人',
+          key: 'type',
+          align: 'center'
+        },
+        {
+          title: '申请时间',
+          key: 'created_at',
+          align: 'center'
+        },
+        {
+          title: '操作',
+          key: 'action',
+          align: 'center',
+          render: (h, params) => {
+            if (this.activeTab == '0') {
+              return h('div', [
+                h('Button', {
+                  props: {
+                    type: 'success'
+                  },
+                  style: {
+                    margin: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.status = '1'
+                      this.recommend_id = params.row.recommend_id
+                      this.cancel()
+                    }
+                  }
+                }, '同意申请'),
+                h('Button', {
+                  props: {
+                    type: 'error'
+                  },
+                  style: {
+                    margin: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.modal = true
+                      this.status = '-1'
+                      this.recommend_id = params.row.recommend_id
+                      console.log(params, '56565')
+                    }
+                  }
+                }, '拒绝申请')
+              ])
             } else {
-              this.$Modal.error({
-                content: response.data.message
-              })
+              return h('div', [
+                h('Button', {
+                  props: {
+                    type: 'error'
+                  },
+                  style: {
+                    margin: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.recommend_id = params.row.recommend_id
+                      this.deleteUser()
+                    }
+                  }
+                }, '删除推荐')
+              ])
             }
-          })
-        }
-      },
-      // 征婚推荐
-      allocation () {
-        if (this.client_id == 0) {
-          return this.$Modal.error({
-            content: '请选择会员'
-          })
-        }
-        let self = this
-        uAxios.post(`admin/home/recommends?user_id=${self.client_id}&photo=${self.photo}`).then((response) => {
-          if (response.data.code === 0) {
-            this.$Message.info('设置成功')
-            location.reload()
-          } else {
-            this.$Modal.error({
-              content: response.data.message
-            })
           }
+        }
+      ],
+      status: '',
+      modal: false,
+      value: '',
+      recommend_id: '',
+      image: '',
+      information: [
+        {id: 250, updatedAt: '数据缺失'},
+        {id: 256, updatedAt: '数据缺失'},
+        {id: 257, updatedAt: '数据缺失'}
+      ],
+      title: '',
+      loading: false,
+    }
+  },
+  methods: {
+    showModel (item) {
+      this.modal1 = true
+      this.image = item
+    },
+    cancel () {
+      this.modal = false
+      let self = this,
+        data = {
+          recommend_id: this.recommend_id,
+          status: this.status,
+          content: this.content
+        }
+      console.log(self.recommend_id, '6666666')
+      uAxios.put(`admin/check/home/recommends/` + self.recommend_id, data)
+        .then(res => {
+          if (res.data.code === 0) this.$Message.info('已处理')
+          this.information.splice(this.feedbackIndex, 1)
         })
-      },
-
-      getlist (page) {
-        let self = this
-        uAxios.get('admin/home/recommends')
-          .then(res => {
-            let result = res.data.data
-            self.information = result.map((item, index) => {
-              return {
-                photo: item.photo,
-                id: item.id,
-                id: item.id,
-                name: item.user.name
-              }
-            })
+    },
+    deleteUser () {
+      let self = this
+      uAxios.delete('admin/home/recommends/' + self.recommend_id).then((response) => {
+        if (response.data.code === 0) {
+          this.$Message.info('删除成功')
+          this.getlist(this.currentPage)
+        } else {
+          this.$Modal.error({
+            content: response.data.message
           })
-        uAxios.get('admin/announcements')
-          .then(res => {
-            let result = res.data.data
-            for (let item of result) {
-              this.messageList.unshift({
-                title: item.title,
-                date: [item.start_time, item.end_time],
-                start_time: item.start_time,
-                end_time: item.end_time,
-                path: item.path,
-                type: item.type,
-                pic: item.pic
-              })
+        }
+      })
+    },
+    getTab (type) {
+      // 获得激活的Tab页
+      this.activeTab = type
+      this.getlist(1)
+    },
+    getlist (page) {
+      let self = this
+      self.loading = true
+      uAxios.get(`admin/home/recommends/v2?page=${page}&status=${self.activeTab}&keyword=${self.searchKeyword}`)
+        .then(res => {
+          let result = res.data.data
+          console.log(result, '9999')
+          self.orgTotal = result.total
+          self.loading = false
+          self.information = result.data.map((item) => {
+            return {
+              circle_avatar: item.user.circle_avatar,
+              created_at: item.created_at,
+              recommend_id: item.id,
+              photo: item.user.photo,
+              user_id: item.user.id,
+              mobile: item.user.mobile,
+              user_name: item.user.name,
+              sex: item.user.sex == 1 ? '男' : '女',
+              rank: item.rank,
+              type: item.user.type == 'single' ? '单身' : '介绍人',
+              is_good_match: item.is_good_match == '0' ? '未认证' : '已认证',
+              address: `${item.province}${item.city}` == '' ? `${item.province} ${item.city}` : '暂无'
             }
           })
-      }
+        })
     },
-    mounted () {
-      //            this.$route.params.id
-      this.id = 1
+    handlePage (num) {
+      // 分页
+      this.currentPage = num
+      this.getlist(num)
+    },
+    handleSearch () {
       this.getlist(1)
     }
+  },
+  mounted () {
+    this.getlist(1)
   }
+}
 </script>
 
-<style>
-  ._bold {
-    font-weight: bold
+<style lang="less">
+  /*@import "../../styles/common";*/
+  .demo-tabs-style1 > .ivu-tabs-card > .ivu-tabs-content {
+    height: 120px;
+    margin-top: -16px;
+  }
+
+  .demo-tabs-style1 > .ivu-tabs-card > .ivu-tabs-content > .ivu-tabs-tabpane {
+    background: #fff;
+    padding: 16px;
+  }
+
+  .demo-tabs-style1 > .ivu-tabs.ivu-tabs-card > .ivu-tabs-bar .ivu-tabs-tab {
+    border-color: transparent;
+  }
+
+  .demo-tabs-style1 > .ivu-tabs-card > .ivu-tabs-bar .ivu-tabs-tab-active {
+    border-color: #fff;
+  }
+
+  .demo-tabs-style2 > .ivu-tabs.ivu-tabs-card > .ivu-tabs-bar .ivu-tabs-tab {
+    border-radius: 0;
+    background: #fff;
+  }
+
+  .demo-tabs-style2 > .ivu-tabs.ivu-tabs-card > .ivu-tabs-bar .ivu-tabs-tab-active {
+    border-top: 1px solid #3399ff;
+  }
+
+  .demo-tabs-style2 > .ivu-tabs.ivu-tabs-card > .ivu-tabs-bar .ivu-tabs-tab-active:before {
+    content: '';
+    display: block;
+    width: 100%;
+    height: 1px;
+    background: #3399ff;
+    position: absolute;
+    top: 0;
+    left: 0;
   }
 </style>
