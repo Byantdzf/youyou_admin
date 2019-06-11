9c2cb02c5255024006e7db1f2b54f81f433a10d5
DZF <dengzhifeng_63@163.com>
2019-06-11 17:19:45 +0800

    测试

--- src/view/auditPhoto/list.vue
+++ src/view/auditPhoto/list.vue
@@ -1,21 +1,5 @@
 <template>
   <div v-model="activeTab">
-    <Card style="margin-bottom: 32px">
-      <div slot="title">
-        <Icon type="ionic"></Icon>
-        点击标签筛选
-        <span
-          style="background-image: linear-gradient(to right, #c8241b, #fc6906);box-shadow:1px 1px 12px #fc9185;padding:2px 7px;color: white;text-align: center;border-radius: 32px;margin-right: 12px;">{{total}}人</span>
-      </div>
-      <div v-for="(item,index) in labels" style="margin-bottom: 12px;display: inline-block;margin-right: 12px">
-        <span style="margin-right: 6px;line-height: 56px;">{{item.title}}</span>
-        <i-switch @on-change="change(item,$event)" :disabled="item.disabled" v-model="item.active"
-                  style="margin-bottom: 2px"/>
-      </div>
-      <div>
-        <Tag v-for="item in count" :key="item" closable @on-close="handleClose2(item,$event)">{{item}}</Tag>
-      </div>
-    </Card>
     <Tabs @on-click="getTab">
       <TabPane :label="item.title" :name="item.jump" v-for="item,index in tab" :key="index">
         <Input
@@ -43,7 +27,7 @@
       ok-text="OK"
       @on-ok="deleteUser"
       no-cancel>
-      <p>是否确认删除佳偶？</p>
+      <p>是否标记为审核失败？</p>
     </Modal>
   </div>
 </template>
@@ -63,8 +47,7 @@
         tab: [{title: '未审核', jump: '0'}, {title: '已审核', jump: '1'}],
         currentPage: 1,
         searchKeyword: '',
-        modal: false, // 弹
-        // 框
+        modal: false, // 弹框
         orgTotal: 0,
         id: '',
         modal1: false,
@@ -72,35 +55,7 @@
         total: '',
         cost: '',
         count: [],
-        jump: {
-          is_vip: 0,
-          is_match: 0,
-          has_maker: 0,
-          type: 'all',
-          sex: 0
-        },
-        labels: [
-          {
-            title: '全部',
-            item: ['vip', '良人佳偶认证', '有跟进红娘', '实名认证', '单身', '介绍人', '男', '女'],
-            active: false,
-            disabled: false
-          },
-          {title: 'vip', item: ['vip'], active: false, disabled: false},
-          {title: '良人佳偶认证', item: ['良人佳偶认证'], active: false, disabled: false},
-          {title: '有跟进红娘', item: ['有跟进红娘'], active: false, disabled: false},
-          {title: '实名认证', item: ['实名认证'], active: false, disabled: false},
-          {title: '单身/介绍人', item: ['单身', '介绍人'], active: false, disabled: false},
-          {title: '男/女', item: ['男', '女'], active: false, disabled: false},
-        ],
         Columns: [
-          {
-            title: '序号',
-            type: 'index',
-            width: 80,
-            align: 'center',
-            sortable: true
-          },
           {
             title: 'ID',
             align: 'center',
@@ -118,7 +73,6 @@
           },
           {
             title: '头像',
-            key: 'updatedAt',
             render: (h, params) => {
               return h('img', {
                 attrs: {
@@ -151,24 +105,9 @@
             key: 'sex'
           },
           {
-            title: '居住省市',
-            align: 'center',
-            key: 'address'
-          },
-          {
-            title: '会员类型',
-            align: 'center',
-            key: 'rank'
-          },
-          {
             title: '单身/介绍人',
             align: 'center',
             key: 'type'
-          },
-          {
-            title: '良人佳偶认证？',
-            align: 'center',
-            key: 'is_good_match'
           },
           {
             title: '加入时间',
@@ -194,6 +133,7 @@
                         this.cost = '1'
                         this.id = params.row.id
                         this.modal1 = true
+                        this.Index = params.index
                       }
                     }
                   }, '同意申请'),
@@ -209,6 +149,7 @@
                         this.cost = '-1'
                         this.id = params.row.id
                         this.modal1 = true
+                        this.Index = params.index
                       }
                     }
                   }, '拒绝申请'),
@@ -228,7 +169,7 @@
                         })
                       }
                     }
-                  }, '用户详情')
+                  }, '查看头像')
                 ])
               } else {
                 return h('div', [
@@ -243,6 +184,7 @@
                       click: () => {
                         this.id = params.row.id
                         this.modal = true
+                        this.Index = params.index
                       }
                     }
                   }, '删除佳偶'),
@@ -262,12 +204,13 @@
                         })
                       }
                     }
-                  }, '用户详情')
+                  }, '查看头像')
                 ])
               }
             }
           }
         ],
+        Index: 0,
         value: '',
         information: [],
         loading: false,
@@ -281,10 +224,10 @@
           data = {
             status: this.cost
           }
-        uAxios.put('admin/check/good/match/' + self.id, data).then((response) => {
+        uAxios.put(`admin/audit/users/${self.id}/photo`, data).then((response) => {
           if (response.data.code === 0) {
             this.$Message.info('操作成功')
-            this.getlist(this.currentPage)
+            this.information.splice(this.Index,1)
           } else {
             this.$Modal.error({
               content: response.data.message
@@ -292,88 +235,10 @@
           }
         })
       },
-      handleClose2 (name, event) {
-        const index = this.count.indexOf(name)
-        this.count.splice(index, 1)
-        this.labels[0].active = false
-        if (this.count.length == 0) {
-          this.labels[0].disabled = false
-        }
-        this.getlist(1)
-        let arr = [
-          {key: 'is_vip', value: 'vip', index: 1},
-          {key: 'is_match', value: '良人佳偶认证', index: 2},
-          {key: 'has_maker', value: '有跟进红娘', index: 3},
-          {key: 'is_approved', value: '实名认证', index: 4}
-        ]
-        for (let item of arr) {
-          switch (this.count.includes(item.value)) {
-            case true:
-              this.labels[item.index].active = true
-              break
-            default:
-              this.labels[item.index].active = false
-          }
-        }
-        switch (this.count.indexOf('男') == -1 && this.count.indexOf('女') == -1) {
-          case true:
-            this.labels[6].active = false
-            break
-          default:
-            this.labels[6].active = true
-        }
-        switch (this.count.indexOf('单身') == -1 && this.count.indexOf('介绍人') == -1) {
-          case true:
-            this.labels[5].active = false
-            break
-          default:
-            this.labels[5].active = true
-        }
-        if (this.count.length != 0) {
-          this.labels[0].disabled = true
-        }
-      },
-      change (item, status) {
-        if (status) {
-          if (item.title === '全部') {
-            for (let index in this.labels) {
-              this.labels[index].active = true
-            }
-          } else {
-            this.labels[0].disabled = true
-          }
-          for (let data of item.item) {
-            this.count.push(data)
-          }
-        } else {
-          if (item.title === '全部') {
-            for (let index in this.labels) {
-              this.labels[index].active = false
-            }
-          }
-          for (let data of item.item) {
-            let index = this.count.indexOf(data)
-            if (index != -1) {
-              this.count.splice(index, 1)
-            }
-          }
-          if (this.count.length == 0) {
-            this.labels[0].disabled = false
-          }
-        }
-        this.getlist(1)
-//                this.$Message.info('开关状态：' + status);
-      },
       createLabel () {
         console.log(this.searchKeyword)
         this.page = 1
         this.getlist(1)
-      },
-      complainList () {
-        // 投诉列表
-        this.$router.push({
-          name: 'complain'
-        })
       },
       filterLabel (page) {
         console.log(this.social)
@@ -386,25 +251,14 @@
         this.getlist(1)
       },
       deleteUser () {
-        let self = this
-        uAxios.delete('admin/good/match/' + self.id).then((response) => {
-          if (response.data.code === 0) {
-            this.$Message.info('删除成功')
-            this.getlist(this.currentPage)
-          } else {
-            this.$Modal.error({
-              content: response.data.message
-            })
-          }
-        })
+        this.cost = '-1'
+        this.ok()
       },
-      getTab (type) {
-        // 获得激活的Tab页
+      getTab (type) { // 获得激活的Tab页
         this.activeTab = type
         this.getlist(1)
       },
-      handlePage (num) {
-        // 分页
+      handlePage (num) { // 分页
         this.currentPage = num
         if (this.social.length == 0) {
           this.getlist(num)
@@ -413,59 +267,16 @@
         }
 
       },
-      searchTitle () {
-        let arr = [
-          {key: 'is_vip', value: 'vip'},
-          {key: 'is_match', value: '良人佳偶认证'},
-          {key: 'has_maker', value: '有跟进红娘'},
-          {key: 'is_approved', value: '实名认证'}
-        ]
-        for (let item of arr) {
-          switch (this.count.includes(item.value)) {
-            case true:
-              this.jump[item.key] = 1
-              break
-            default:
-              this.jump[item.key] = 0
-          }
-        }
-        if (this.count.includes('男')) {
-          this.jump.sex = '1'
-        } else if (this.count.includes('女')) {
-          this.jump.sex = '2'
-        } else {
-          this.jump.sex = '0'
-        }
-        if (this.count.includes('男') && this.count.includes('女')) {
-          this.jump.sex = '0'
-        }
-        if (this.count.includes('单身')) {
-          this.jump.type = 'single'
-        } else if (this.count.includes('介绍人')) {
-          this.jump.type = 'marriage'
-        } else {
-          this.jump.type = ''
-        }
-        if (this.count.includes('单身') && this.count.includes('介绍人')) {
-          this.jump.type = ''
-        }
-        console.log(this.jump)
-      },
       getlist (page) {
-        let self = this,
-          jump = ''
-        self.searchTitle()
+        let self = this
         self.loading = true
-        for (let item in self.jump) {
-          jump = jump + `&${item}=${self.jump[item]}`
-        }
-        uAxios.get('admin/good/matches?page=' + page + '&keyword=' + self.searchKeyword + jump + '&status=' + self.activeTab)
+        uAxios.get('admin/audit/users/photo?page=' + page + '&keyword=' + self.searchKeyword + '&status=' + self.activeTab)
           .then(res => {
             let result = res.data.data
             self.total = res.data.data.total
             self.information = result.data.map((item) => {
               return {
-                avatar: item.avatar,
+                avatar: item.photo,
                 created_at: item.created_at,
                 id: item.id,
                 mobile: item.mobile,
@@ -497,14 +308,7 @@
       }
     },
     mounted () {
-      // if (Cookies.get('admin_type') === 'matcher') {
-      //     return this.$router.push({
-      //         name: 'error-403',
-      //         params: ''
-      //     });
-      // }
       this.getlist(1)
-      // this.$store.commit('updateMenulist');
     }
   }
 </script>
